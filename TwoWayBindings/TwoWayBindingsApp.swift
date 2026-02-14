cdfa07c9366692541893ea25769210ce89de3b23
Burak Ilgin <burakilginnn@gmail.com>
2026-02-14 15:44:48 +0300

    Initial Commit

---
+++ TwoWayBindings/TwoWayBindingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingsApp.swift
+//  TwoWayBindings
+//
+//  Created by Burak Ilgın on 14.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
